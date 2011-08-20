05ac82f27272646dcbf24c8b732cb57b4f932401
Kunal Kerkar <kunalkerkar@gmail.com>
2011-08-20 23:02:59 +0530

    Jingle channel in TCP

--- mod_jinglechannel.lua
+++ mod_jinglechannel.lua
@@ -1,61 +1,159 @@
 --Jingle Channel Implementation
 
-socket = require "socket"
-local localport = module:get_option_number("jingle_channel_read_port") or 35800;
-local remoteport = module:get_option_number("jingle_channel_write_port") or 35802;
+local _G = _G;
+local prosody = _G.prosody;
+local hosts = prosody.hosts;
 
 local connlistener_register = require "net.connlisteners".register;
 
-local udp_localport_rtp = socket.udp();
-local udp_localport_rtcp = socket.udp();
-local udp_remoteport_rtp = socket.udp();
-local udp_remotrport_rtcp = socket.udp();
+local localport = module:get_option_number("jingle_channel_read_port") or 35800;
+local remoteport = module:get_option_number("jingle_channel_write_port") or 35802;
 
--- RTP localport
-function read_rtp_localport(data)
-    udp_localport_rtp:setsockname("*", data.localport);
-    udp_localport_rtp:settimeout(data.expire);
-    return udp_localport_rtp:receivefrom()
+local console_listener_localport_rtp = {default_port = localport; default_mode = "*a"; default_interface = "*" };
+local console_listener_remoteport_rtp = {default_port = remoteport; default_mode = "*a"; default_interface = "*" };
+local console_listener_localport_rtcp = {default_port = localport+1; default_mode = "*a"; default_interface = "*" };
+local console_listener_remoteport_rtcp = {default_port = remoteport+1; default_mode = "*a"; default_interface = "*" };
+
+
+local def_env = {};
+local default_env_mt = { __index = def_env };
+
+prosody.console = { commands = commands, env = def_env };
+
+console = {};
+function console:new_session(conn)
+    local w = function(s) conn:write(s); end;
+    local session = { conn = conn;
+        send = function (t) w(tostring(t)); end;
+        disconnect = function () conn:close() end;
+    };
+
+    return session
 end
 
-function write_rtp_localport(msg, ip, port)
-    return udp_localport_rtp:sendto(msg, ip, port)
+local sessions = {  localport_rtp = nil;
+                    localport_rtcp = nil;
+                    remoteport_rtp = nil;
+                    remoteport_rtcp = nil;
+                };
+
+-- Localport RTP connection
+
+function console_listener_localport_rtp.onconnect(conn)
+    local session = console:new_session(conn);
+    if sessions.localport_rtp == nil then
+        sessions.localport_rtp = session;
+    end
+end
+        
+function console_listener_localport_rtp.onincoming(conn, data)
+    local session = sessions.remoteport_rtp;
+
+    (function(session, data)
+        if session  then
+            session.send(data);
+        end
+    end)(session, data);
 end
 
---RTP remoteport
-function read_rtp_remoteport(data)
-    udp_remoteport_rtp:setsockname("*", data.remoteport);
-    udp_remoteport_rtp:settimeout(data.expire);
-    return udp_remoteport_rtp:receivefrom()
+function console_listener_localport_rtp.ondisconnect(conn, err)
+    local session = sessions.localport_rtp;
+    if session then
+        session.disconnect();
+        sessions.localport_rtp = nil;
+    end
 end
 
-function write_rtp_remoteport(msg, ip, port)
-    return udp_remoteport_rtp:sendto(msg, ip, port)
+
+-- Localport RTCP connection
+
+function console_listener_localport_rtcp.onconnect(conn)
+    local session = console:new_session(conn);
+    if sessions.localport_rtcp = nil then
+        sessions.localport_rtcp = session;
+    end
 end
 
---RTCP localport
-function read_rtcp_localport(data)
-    udp_localport_rtp:setsockname("*", data.localport+1);
-    udp_localport_rtp:settimeout(data.expire);
-    return udp_localport_rtp:receivefrom()
+function console_listener_localport_rtcp.onincoming(conn, data)
+    local session = sessions.remoteport_rtcp;
+
+    (function(session, data)
+        if session then
+            session.send(data);
+        end
+    end)(session, data);
 end
 
-function write_rtcp_localport(msg, ip, port)
-    return udp_localport_rtcp:sendto(msg, ip, port)
+function console_listener_localport_rtcp.ondisconnect(conn, err)
+    local session = sessions.localport_rtcp;
+    if session then
+        session.disconnect();
+        sessions.localport_rtcp = nil;
+    end
 end
 
---RTCP remoteport
-function read_rtcp_remoteport(data)
-    udp_remoteport_rtcp:setsockname("*", data.remoteport+1);
-    udp_remoteport_rtcp:settimeout(data.expire)
-    return udp_remoteport_rtp:receivefrom()
+-- Remoteport RTP connection
+
+function console_listener_remoteport_rtp.onconnect(conn)
+    local session = console:new_session(conn);
+    if sessions.remoteport_rtp = nil then
+        sessions.remoteport_rtp = session;
+    end
 end
 
-function write_rtcp_remoteport(msg, ip, port)
-    return udp_remoteport_rtcp:sendto(msg, ip, port)
+function console_listener_remoteport_rtp.onincoming(conn, data)
+    local session = sessions.localport_rtp;
+
+    (function(session, data)
+        if session then
+            session.send(data);
+        end
+    end)(session, data);
 end
 
-module:hook('jingle/channel', function(data)
+function console_listener_remoteport_rtp.ondisconnect(conn, err)
+    local session = sessions.remoteport_rtp;
+    if session then
+        session.disconnect();
+        sessions.remoteport_rtp = nil;
+    end
+end
 
-    -- Listening on RTP localport
-    local msg, ip, port = read_rtp_localport(data);
+
+-- Remoteport RTCP connection
+
+function console_listener_remoteport_rtcp.onconnect(conn)
+    local session = concole:new_session(conn);
+    if sessions.remoteport_rtcp = nil then
+        sessions.remoteport_rtcp = session;
+    end
+end
+
+function console_listener_remoteport_rtcp.onincoming(conn, data)
+    local session = sessions.localport_rtcp;
+
+    (function(session, data)
+        if session then
+            session.send(data);
+        end
+    end)(session, data);
+end
+
+function console_listener_remoteport_rtcp.ondisconnect(conn, err)
+    local session = sessions.remoteport_rtcp;
+    if session then
+        session.disconnect();
+        sessions.remoteport_rtcp = nil;
+    end
+end
+
+connlisteners_register('console', console_listener_localport_rtp);
+connlisteners_register('console', console_listener_localport_rtcp);
+connlisteners_register('console', console_listener_remoteport_rtp);
+connlisteners_register('console', console_listener_remoteport_rtcp);
+
+
+prosody.net_activate_ports("console", "console", {localport}, "tcp");
+prosody.net_activate_ports("console", "console", {localport+1}, "tcp");
+prosody.net_activate_ports("console", "console", {remoteport}, "tcp");
+prosody.net_activate_ports("console", "console", {remoteport+1}, "tcp");
